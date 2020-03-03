76220fe7f4788dea9259dfa08830bb2c3985b99f
Mina Rady <minarady@gmail.com>
2020-03-03 10:55:03 +0100

    modified payload to be dynamic and loop on all radio interfaces

--- dynamic_functions_array_final.c
+++ dynamic_functions_array_final.c
@@ -1,44 +1,46 @@
 /******************************************************************************
-
-                            Online C Compiler.
-                Code, Compile, Run and Debug C program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
+Template for dynamic radio allocation for openwsn
+@minarady1
+mina1.rady@orange.com
 *******************************************************************************/
 
 #include <stdio.h>
 
 typedef void                (*radio_cb_t)(void);
-
-
-void rad1()
-{
-    printf("Tx from Radio 1\n");
-}
+// ... more types to be added in accordance with the API
 
 enum radios
 {
     RADIO_1,
     RADIO_2
 };
+// 807 release defined these two parameters in board_info.h
 int MAX_RADIOS = 2;
 int SELECTED_RADIO = RADIO_2;
 
-void rad2()
-{
-    printf("Tx from Radio 2\n");
-}
-
+// Radio specific functions. These will be placed in separate .c files
+// such as radio_RADIO1.c and radio_RADIO2.c
 void rad1_init()
 {
     printf("Initializing  Radio 1\n");
 }
 
+void tx_radio_rad1()
+{
+    printf("Tx from Radio 1\n");
+}
 
 void rad2_init()
 {
     printf("Initializing  Radio 2\n");
 }
+
+void tx_radio_rad2()
+{
+    printf("Tx from Radio 2\n");
+}
+
+
 typedef struct {
     radio_cb_t tx_radio;
     radio_cb_t init_radio;
@@ -47,12 +49,13 @@ typedef struct {
 int main()
 {
     radio_functions_t dyn_funcs [MAX_RADIOS];
-    
-    dyn_funcs [0].tx_radio=rad1;
-    dyn_funcs [1].tx_radio=rad2;
-    dyn_funcs [0].init_radio=rad1_init;
-    dyn_funcs [1].init_radio=rad2_init;
 
+    dyn_funcs [0].init_radio    =   rad1_init;
+    dyn_funcs [1].init_radio    =   rad2_init;   
+    dyn_funcs [0].tx_radio      =   tx_radio_rad1;
+    dyn_funcs [1].tx_radio      =   tx_radio_rad2;
+    
+    // Initialize and transmit from selected radio
     dyn_funcs [SELECTED_RADIO].init_radio();
     dyn_funcs [SELECTED_RADIO].tx_radio();
 
---
+++ dynamic_functions_array_with_payload.c
@@ -0,0 +1,99 @@
+/******************************************************************************
+Template for dynamic radio allocation for openwsn
+@minarady1
+mina1.rady@orange.com
+*******************************************************************************/
+
+#include <stdio.h>
+
+// radio functions callback types declaration
+typedef void                (*radio_cb_t)(void);
+typedef void                (*radio_tx_cb_t)(char * str);
+// ... more types to be added in accordance with the API
+
+// the template for radio function callbacks
+// inspired from release FW_807 bsp/common/openradio.h
+typedef struct {
+    radio_tx_cb_t tx_radio;
+    radio_cb_t init_radio;
+} radio_functions_t;
+
+enum radios
+{
+    RADIO_1,
+    RADIO_2,
+    RADIO_3
+};
+
+// select which radio to use for this slot
+// this will be initialized in ti1ORri1
+int SELECTED_RADIO = RADIO_1;
+
+// 807 release defined these two parameters in board_info.h
+// this will be used to create the matrix size
+int MAX_RADIOS = 3;
+
+// Radio specific functions. These will be placed in separate .c files
+// such as radio_RADIO1.c and radio_RADIO2.c
+
+//======= RADIO 1 Driver ================
+void rad1_init()
+{
+    printf("Initializing  Radio 1\n");
+}
+
+void tx_radio_rad1(char * str)
+{
+    printf("Tx from Radio 1\n");
+    printf(str);
+}
+
+//======= RADIO 2 Driver ================
+void rad2_init()
+{
+    printf("Initializing  Radio 2\n");
+}
+
+void tx_radio_rad2(char * str)
+{
+    printf("Tx from Radio 2\n");
+    printf(str);
+}
+
+//======= RADIO 3 Driver ================
+void rad3_init()
+{
+    printf("Initializing  Radio 3\n");
+}
+
+void tx_radio_rad3(char * str)
+{
+    printf("Tx from Radio 3\n");
+    printf(str);
+}
+int main ()
+{
+    //selected radio 0=RADIO1 or 1=RADIO2
+    radio_functions_t dyn_funcs [MAX_RADIOS];
+    
+    // initializing the lookup table for radio function callbacks
+    dyn_funcs [0].init_radio    =   rad1_init;
+    dyn_funcs [1].init_radio    =   rad2_init;   
+    dyn_funcs [2].init_radio    =   rad3_init;   
+    
+    dyn_funcs [0].tx_radio      =   tx_radio_rad1;
+    dyn_funcs [1].tx_radio      =   tx_radio_rad2;
+    dyn_funcs [2].tx_radio      =   tx_radio_rad3;
+    
+    // Initialize and transmit from selected radio
+    for (int i =0; i< MAX_RADIOS; i++)
+    {
+        SELECTED_RADIO = i;
+        dyn_funcs [SELECTED_RADIO].init_radio();
+        char payload [12]= "\tmy packet";
+        dyn_funcs [SELECTED_RADIO].tx_radio(payload);   
+    }
+
+    return 0;
+}
+
